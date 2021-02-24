823b4f3e4d6225bc5ec56f8793a65a7442fcd203
Vivek <vivekascoder@gmail.com>
2021-02-24 18:08:46 +0530

    Added Glyph on Workspace and added multi monitor support.

--- polybar/v3/launch.sh
+++ polybar/v3/launch.sh
@@ -3,7 +3,13 @@
 killall -q polybar
 
 export POLYBAR_PATH="$HOME/.config/polybar/v3"
-polybar bar -c $POLYBAR_PATH/config.ini 2>&1 | tee -a /tmp/poybar1.log & disown
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload bar -c $POLYBAR_PATH/config.ini  &
+    done
+else
+    polybar bar -c $POLYBAR_PATH/config.ini 2>&1 | tee -a /tmp/poybar1.log & disown
+fi
 
 echo "POLYBAR LAUNCHED."
 
